208d60e487c2686d31db893244955da3bdf29353
Hansani Gunathilaka <hansinima20003@gmail.com>
2023-10-28 01:57:23 +0530

    Add files via upload

---
+++ Basics/Switch.js
@@ -0,0 +1,26 @@
+
+let day = "Wednesday";
+
+switch(day) {
+    case 'Monday' : console.log("Let's go to work!"); break;  
+    case 'Tuesday' : console.log("Library day!"); break;
+    case 'Wednesday' : console.log("Hurry up! Go to the gym"); break;
+    case 'Thursday' : console.log("Let's meat the boyfriend"); break;
+    case 'Friday' : console.log("Again a working day!"); break;
+    case 'Saturday' : console.log("Let's read a book"); break;
+    case 'Sunday' : console.log("Finally a free day!!");
+
+}
+
+//Template literal
+
+let num1 =  10, num2 = 5;
+
+let result = num1 + num2;
+
+console.log(`The addition of ${num1} and ${num2} is ${result}`); //${}
+
+console.log(`Hansani
+Nimasha`); //using `` we can print newline without using \n
+
+console.log("Hansani", " Nimasha", " Gunathilaka");
